68c72cf59b7b509d05a435b79b2706f0122b4bf5
Chauncey <50437772+zxbzxb180@users.noreply.github.com>
2022-05-09 20:11:37 +0800

    看图猜电影网站

--- beego_web/controllers/default.go
+++ beego_web/controllers/default.go
@@ -1,6 +1,9 @@
 package controllers
 
 import (
+	"beego_web/models"
+	"encoding/json"
+	"errors"
 	"github.com/astaxie/beego"
 )
 
@@ -9,8 +12,58 @@ type MainController struct {
 }
 
 func (c *MainController) Get() {
-	website := c.GetString("website")
-	c.Data["Website"] = website
-	c.Data["Email"] = "616529325@qq.com"
-	c.TplName = "index.tpl"
+	var subject models.Subject
+	err := func() error {
+		id, err := c.GetInt("id")
+		beego.Info(id)
+		if err != nil {
+			id = 1
+		}
+		subject, err = models.GetSubject(id)
+		if err != nil {
+			return errors.New("subject error")
+		}
+		return nil
+	}()
+
+	if err != nil {
+		c.Ctx.WriteString("wrong params")
+	}
+
+	var option map[string]string
+	if err = json.Unmarshal([]byte(subject.Option), &option); err != nil {
+		c.Ctx.WriteString("wrong params, json decode")
+	}
+	c.Data["ID"] = subject.Id
+	c.Data["Option"] = option
+	c.Data["Img"] = "/static" + subject.Img
+	c.TplName = "guess.tpl"
+}
+
+func (c *MainController) Post() {
+	var subject models.Subject
+	err := func() error {
+		id, err := c.GetInt("id")
+		beego.Info(id)
+		if err != nil {
+			id = 1
+		}
+		subject, err = models.GetSubject(id)
+		if err != nil {
+			return errors.New("subject not exist")
+		}
+		return nil
+	}()
+
+	if err != nil {
+		c.Ctx.WriteString("wrong params")
+	}
+	answer := c.GetString("key")
+	right := models.Answer(subject.Id, answer)
+
+	c.Data["Right"] = right
+	c.Data["Next"] = subject.Id + 1
+	c.Data["ID"] = subject.Id
+	c.TplName = "guess.tpl"
+
 }
--- beego_web/routers/router.go
+++ beego_web/routers/router.go
@@ -6,6 +6,6 @@ import (
 )
 
 func init() {
-	//beego.Router("/", &controllers.MainController{})
-	beego.Include(&controllers.UserController{})
+	beego.Router("/", &controllers.MainController{})
+	//beego.Include(&controllers.UserController{})
 }
